65f2d1ddf6b224a45926d7010a9a9aea274dafec
Focus <batieatworld@gmail.com>
2016-01-02 13:51:59 +0000

    Small fix to build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 VERSION="1.0.0"
 ./scripts/compile.sh
+npm install --production
 electron-packager ./ BibtexImport --platform=darwin --arch=x64 --version=0.36.0 --icon=./icons/icon.icns --overwrite --out=./bins/ --app-version=$VERSION --ignore="(LICENCE|src|README.md|icons/|support/|bins/|scripts/)"
 cd $STARTDIR
