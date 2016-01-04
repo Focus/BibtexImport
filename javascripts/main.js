856644b78cf3da79200adfc64e715e09bfcedf33
Focus <batieatworld@gmail.com>
2016-01-04 22:14:53 +0000

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+var version = "1.0.0";
+var platform = window.navigator.platform;
+var link = document.getElementById("link");
+var small = document.getElementById("small");
+
+if (platform === "MacIntel") {
+  small.innerHTML = ".dmg";
+  link.href = "https://github.com/Focus/BibtexImport/releases/download/v" + version + "/BibtexImport.app.zip";
+  link.innerHTML += "OS X 64bit";
+}
