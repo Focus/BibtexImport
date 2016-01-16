04088322bcb6425aadb7a9149f08e253e143c776
Focus <batieatworld@gmail.com>
2016-01-16 13:24:48 +0000

    Update os-detect.js

--- javascripts/os-detect.js
+++ javascripts/os-detect.js
@@ -9,7 +9,26 @@ if (platform.os.family === "OS X") {
   link.innerHTML += "OS X 64bit";
 }
 else if (platform.os.family.indexOf("Windows") != -1 && platform.os.architecture === 64){
-  small.innerHTML  = ".exe";
-  link.href = "https://github.com/Focus/BibtexImport/releases/download/v" + version + "/BibtexImportSetup64.exe";
-  link.innerHTML += "Win 64bit";
+  if(platform.os.architecture === 64){
+    small.innerHTML  = ".exe";
+    link.href = "https://github.com/Focus/BibtexImport/releases/download/v" + version + "/BibtexImportSetup64.exe";
+    link.innerHTML += "Win 64bit";
+  }
+  else {
+    small.innerHTML  = ".exe";
+    link.href = "https://github.com/Focus/BibtexImport/releases/download/v" + version + "/BibtexImportSetup32.exe";
+    link.innerHTML += "Win 32bit";
+  }
+}
+else if (/Ubuntu|Debian|Fedora|Red Hat|SuSE/.test(platform.os.family)){
+  if(platform.os.architecture === 64){
+    small.innerHTML  = ".deb";
+    link.href = "https://github.com/Focus/BibtexImport/releases/download/v" + version + "/BibtexImportSetup64.deb";
+    link.innerHTML += "Linux 64bit";
+  }
+  else {
+    small.innerHTML  = ".deb";
+    link.href = "https://github.com/Focus/BibtexImport/releases/download/v" + version + "/BibtexImportSetup32.deb";
+    link.innerHTML += "Linux 32bit";
+  }
 }
