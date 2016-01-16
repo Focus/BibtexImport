697d88f955a99ccd4890b21bc90d09c1e46ef286
Focus <batieatworld@gmail.com>
2016-01-16 12:54:37 +0000

    Update os-detect.js

--- javascripts/os-detect.js
+++ javascripts/os-detect.js
@@ -8,7 +8,7 @@ if (platform.os.family === "OS X") {
   link.href = "https://github.com/Focus/BibtexImport/releases/download/v" + version + "/BibtexImport.dmg";
   link.innerHTML += "OS X 64bit";
 }
-else if (platform.os.family.indexOf("Windows") != -1 && platform.architecture === 64){
+else if (platform.os.family.indexOf("Windows") != -1 && platform.os.architecture === 64){
   small.innerHTML  = ".exe";
   link.href = "https://github.com/Focus/BibtexImport/releases/download/v" + version + "/BibtexImportSetup64.exe";
   link.innerHTML += "Win 64bit";
